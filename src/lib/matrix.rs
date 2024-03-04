75fc1ccd6943ac448abe22ab85dc22a96ba3138c
Pranav AVN <pranavavn0811@gmail.com>
2024-03-04 23:33:31 +0530

    Added some basic Matrix functions

--- src/lib/matrix.rs
+++ src/lib/matrix.rs
@@ -1,3 +1,4 @@
+use rand::thread_rng;
 pub struct Matrix{
     pub rows: usize,
     pub cols: usize,
@@ -11,5 +12,51 @@ impl Matrix{
             cols,
             data: vec![vec![0.0, cols], rows],
         }
+    }
+
+    pub fn random(rows:usize, cols: usize) -> Matrix{
+        let mut rng = thread_rng();
+
+        let mut res = Matrix::zeros(rows, cols);
+
+        for i in 0..rows{
+            for j in 0..cols{
+                res.data[i][j] = rng.gen::<f64>() * 2.0 - 1.0;
+            } 
+        }
+        res
+    }
+
+    pub fn multiply(&mut self, other: &Matrix) -> Matrix{
+        if self.cols != other.rows{
+            panic!("Dimension Error")
+        }
+
+        let mut res = Matrix::zeros(self.rows, other.cols);
+        for i in 0..self.rows{
+            for j in 0..other.cols{
+                let mut sum = 0.0;
+                for k in 0..self.cols{
+                    sum += self.data[i][k] * other.data[k][j];
+                }
+                res. data[i][j] = sum;
+            }
+        }
+        res
+    }
+
+    pub fn add(&mut self, other: &Matrix) -> Matrix{
+        if self.rows != other.rows || self.cols != other.cols{
+            panic!("Dimension Error")
+        }
+
+        let mut res = Matrix::zeros(self.rows, self.cols);
+        for i in 0..self.rows{
+            for j in 0..self.cols{
+                res. data[i][j] = self.data[i][j] + other.data[i][j];
+            }
+        }
+        res
+
     }
 }
