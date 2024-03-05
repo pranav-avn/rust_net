f32781a3e7ba0d678e895d3ca7ab5542adafa8b6
Pranav AVN <pranavavn0811@gmail.com>
2024-03-05 20:46:56 +0530

    Added more activation functions

--- src/activations.rs
+++ src/activations.rs
@@ -10,3 +10,18 @@ pub const SIGMOID: Activation = Activation{
     function: &|x| 1.0/(1.0 + E.powf(-x)),
     derivative: &|x| x * (1.0 - x),
 };
+
+pub const IDENTITY: Activation = Activation{
+    function: &|x| x,
+    derivative: &|_| 1.0,
+};
+
+pub const TANH: Activation = Activation{
+    function: &|x| x.tanh(),
+    derivative: &|x| 1.0 - (x.powi(2)),
+};
+
+pub const RELU: Activation = Activation{
+    function: &|x| x.max(0.0),
+    derivative: &|x| if x > 0.0 {1.0} else {0.0},
+};
