d35d972f83c53784d4af12f72894a603c81c27d8
Pranav AVN <pranavavn0811@gmail.com>
2024-03-05 19:38:34 +0530

    Refactored Code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use activations::SIGMOID;
+use network::Network;
+
+pub mod activations;
+pub mod matrix;
+pub mod network;
+
+
+// XOR Test
+// 0,0 -> 0
+// 0,1 -> 1
+// 1,0 -> 1
+// 1,1 -> 0
+
 fn main() {
-    println!("Hello, world!");
+    let inputs = vec![
+        vec![0.0, 0.0],
+        vec![0.0, 1.0],
+        vec![1.0, 0.0],
+        vec![1.0, 1.0],
+    ];
+
+    let targets = vec![
+        vec![0.0],
+        vec![1.0],
+        vec![1.0],
+        vec![0.0]
+    ];
+    
+    let mut network = Network::new(vec![2,3,1], 0.5, SIGMOID);
+
+    network.train(inputs, targets, 1000);
+
+    println!("{:?}", network.feed_forward(vec![0.0, 0.0]));
+	println!("{:?}", network.feed_forward(vec![0.0, 1.0]));
+	println!("{:?}", network.feed_forward(vec![1.0, 0.0]));
+	println!("{:?}", network.feed_forward(vec![1.0, 1.0]));
 }
