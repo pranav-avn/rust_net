940f29e7df6941f7efa0707330c8eb6dcb51a489
Pranav AVN <pranavavn0811@gmail.com>
2024-03-05 20:50:21 +0530

    Add Sample Output for XOR

--- src/main.rs
+++ src/main.rs
@@ -31,8 +31,14 @@ fn main() {
 
     network.train(inputs, targets, 10000);
 
-    println!("{:?}", network.feed_forward(vec![0.0, 0.0]));
-	println!("{:?}", network.feed_forward(vec![0.0, 1.0]));
-	println!("{:?}", network.feed_forward(vec![1.0, 0.0]));
-	println!("{:?}", network.feed_forward(vec![1.0, 1.0]));
+    println!("0 and 0 {:?}", network.feed_forward(vec![0.0, 0.0]));
+	println!("0 and 1 {:?}", network.feed_forward(vec![0.0, 1.0]));
+	println!("1 and 0 {:?}", network.feed_forward(vec![1.0, 0.0]));
+	println!("1 and 1 {:?}", network.feed_forward(vec![1.0, 1.0]));
 }
+
+// Outputs
+// 0 and 0 [0.0011161326398356243]
+// 0 and 1 [0.9836577235752275]
+// 1 and 0 [0.9854072704733727]
+// 1 and 1 [0.020678408884407383]
